6fceec6c7e9e25261fb7f48393c98d117a6d1fb7
Patryyyy <78462427+patryyyy@users.noreply.github.com>
2023-10-06 18:38:01 +0800

    fix: Improve the time format (#1)

--- bin/feishu_bot.dart
+++ bin/feishu_bot.dart
@@ -4,12 +4,15 @@ import 'package:cron/cron.dart';
 
 import 'package:feishu_bot/feishu_bot.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  // 获取环境变量
   String CITY = Platform.environment["CITY"] ?? "";
   String GAODE_API_KEY = Platform.environment["GAODE_API_KEY"] ?? "";
   String FEISHU_BOT_URL = Platform.environment["FEISHU_BOT_URL"] ?? "";
-  String FEISHU_BOT_SIGNATURE = Platform.environment["FEISHU_BOT_SIGNATURE"] ?? "";
+  String FEISHU_BOT_SIGNATURE =
+      Platform.environment["FEISHU_BOT_SIGNATURE"] ?? "";
 
+  // 判断是否为空
   if (GAODE_API_KEY.isEmpty ||
       FEISHU_BOT_SIGNATURE.isEmpty ||
       FEISHU_BOT_URL.isEmpty ||
@@ -19,24 +22,26 @@ void main(List<String> args) {
     exit(1);
   }
 
+  // 获取城市adcode
+  Adcode adcode = await Adcode.getAdcode(GAODE_API_KEY, CITY);
+
   print("Started successfully at ${DateTime.now().toString()}");
 
   final cron = Cron();
 
   cron.schedule(Schedule.parse("0 0 9,12,15,18,21 * * *"), () async {
-    await sendWeatherInfo(
-        CITY, GAODE_API_KEY, FEISHU_BOT_SIGNATURE, FEISHU_BOT_URL);
+    await sendWeatherInfo(adcode.adcode, adcode.name, GAODE_API_KEY,
+        FEISHU_BOT_SIGNATURE, FEISHU_BOT_URL);
   });
 }
 
-Future<void> sendWeatherInfo(String CITY, String GAODE_API_KEY,
+Future<void> sendWeatherInfo(int city, String name, String GAODE_API_KEY,
     String FEISHU_BOT_SIGNATURE, String FEISHU_BOT_URL) async {
-  Adcode adcode = await Adcode.getAdcode(GAODE_API_KEY, CITY);
   WeatherData weatherData =
-      await WeatherData.getWeatherData(GAODE_API_KEY, adcode.adcode);
+      await WeatherData.getWeatherData(GAODE_API_KEY, city);
   FeishuBot feishuBot = FeishuBot(FEISHU_BOT_URL, FEISHU_BOT_SIGNATURE);
 
   await feishuBot.sendMessage(Message("Hi, 新的天气来喽"));
   await feishuBot.sendMessage(MessageCard(
-      adcode.name, weatherData.toString(), "https://www.msn.cn/zh-cn/weather"));
+      name, weatherData.toString(), "https://www.msn.cn/zh-cn/weather"));
 }
